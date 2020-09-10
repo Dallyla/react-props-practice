5fda7c7aed7cb39a5c7af7a869e624f5d73f9388
Dallyla Oliveira <51199118+Dallyla@users.noreply.github.com>
2020-09-10 20:29:50 -0300

    import contacts

--- src/index.js
+++ src/index.js
@@ -2,27 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Card from "./components/Card";
+import contacts from "../src/contacts";
 
 ReactDOM.render(
   <div>
     <App />
     <Card
-      name="Beyoncé"
-      image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      phone="+123 456 789"
-      email="b@beyonce.com"
+      name={contacts[0].name}
+      image={contacts[0].imgURL}
+      phone={contacts[0].phone}
+      email={contacts[0].email}
     />
     <Card
-      name="Jack Bauer"
-      image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      phone="+987 654 321"
-      email="jack@nowhere.com"
+      name={contacts[1].name}
+      image={contacts[1].imgURL}
+      phone={contacts[1].phone}
+      email={contacts[1].email}
     />
     <Card
-      name="Chuck Norris"
-      image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      phone="+918 372 574"
-      email="gmail@chucknorris.com"
+      name={contacts[2].name}
+      image={contacts[2].imgURL}
+      phone={contacts[2].phone}
+      email={contacts[2].email}
     />
   </div>,
   document.getElementById("root")
